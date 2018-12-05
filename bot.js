bc518f01fb8a847adf63fac1f63cfaba59751e3f
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-12-05 23:27:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-    var embed = new Discord.RichEmbed()
-    .setThumbnail(member.user.avatarURL)
-  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
-    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
-    .setColor('RANDOM')
+        message.channel.send('take your time :heart:')
 var channel = member.guild.channels.find('name', 'chat')
-if (!channel) return;
-channel.send({embed : embed});
 });
 client.login(process.env.BOT_TOKEN);
