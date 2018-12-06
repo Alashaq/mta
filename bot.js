229b719ebca30daac61aee5f73570a0e21278348
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-12-06 14:28:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'chat');
-      if (!channel) return; 
-           message.channel.send(`**Wééééééélcome To overHype Server :bear: :heart: :champagne_glass:  .**`)
+client.on('guildMemberAdd', member=> {
+
+    client.channels.get("509728837546803202").send(`***__ Wééééééélcome To overHype Server :bear: :heart: :champagne_glass:   ${member}___***`);
     });
 client.login(process.env.BOT_TOKEN);
